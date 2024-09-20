6ac2d1f4c70ff58027a6939caa9aa1e213dc80ed
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2024-09-20 10:46:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const paragraph = document.getElementById('paragraph');
+const button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    paragraph.style.display = 'none';
+})
