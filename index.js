17c393658b138bef85ff1a0599a3cb9452bac850
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2024-09-21 11:19:38 +0300

    landing page

--- index.js
+++
@@ -1,6 +0,0 @@
-const paragraph = document.getElementById('paragraph');
-const button = document.getElementById('button');
-
-button.addEventListener('click', () => {
-    paragraph.style.display = 'none';
-})
